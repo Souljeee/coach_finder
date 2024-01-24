9213e2a1b9a9b51b992f17ee9fa6cb144f9a4292
Souljeee <drobitek76106055@gmail.com>
2024-01-24 20:05:13 +0300

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              Center(
+                child: Text('Hello world!'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
