be038e662d6678e171a9121121cc1e2b7b1e8847
Souljeee <drobitek76106055@gmail.com>
2024-01-31 20:25:47 +0300

    feat(auth_bloc): implements auth bloc with empty requests

--- lib/features/auth/bloc/auth_bloc.dart
+++ lib/features/auth/bloc/auth_bloc.dart
@@ -4,12 +4,45 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
 part 'auth_event.dart';
+
 part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   AuthBloc() : super(const AuthState.unauthorized()) {
-    on<AuthEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+    on<AuthEvent>(
+      (event, emit) => event.map(
+        signIn: (event) => _onSignIn(event, emit),
+        signOut: (_) => _onSignOut(emit),
+      ),
+    );
+  }
+
+  Future<void> _onSignIn(
+    _SignInEvent event,
+    Emitter<AuthState> emit,
+  ) async {
+    try{
+      emit(const AuthState.checkingAuth());
+
+      // TODO: request
+
+      emit(const AuthState.authorized());
+    }catch(e, s){
+      addError(e, s);
+
+      emit(const AuthState.checkingAuth());
+    }
+  }
+
+  Future<void> _onSignOut(Emitter<AuthState> emit) async {
+    try{
+      // TODO: request
+
+      emit(const AuthState.unauthorized());
+    }catch(e, s){
+      addError(e, s);
+
+      emit(const AuthState.unauthorized());
+    }
   }
 }
